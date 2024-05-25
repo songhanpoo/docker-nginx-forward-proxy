50efcbaabafa5a9e0eae8ba60d2c961d4a4d1adc
Songhanpoo <songhanpoo@gmail.com>
2024-05-25 21:24:53 +0700

    :tada: add feature toggler on/off for HTTP_PASSWD

--- nginx-core/docker-entrypoint.sh
+++ nginx-core/docker-entrypoint.sh
@@ -2,14 +2,21 @@
 set -eu
 
 RESOLVER=${RESOLVER:-8.8.8.8}
-if [ -n "$RESOLVER" ] && [ -n "$LISTTEN_PORT" ] && [ -n "$HTTP_PASSWD" ]; then
+HTTP_PASSWD=${HTTP_PASSWD:-}
+if [ -n "$RESOLVER" ] || [ -n "$LISTTEN_PORT" ] ; then
     echo "Applying: resolver $RESOLVER;"
     echo "System is listening on port $LISTTEN_PORT;"
     echo "Configuration httppasswd is locating at /usr/local/nginx/conf/httppasswd"
-
-    touch /usr/local/nginx/conf/httppasswd
     envsubst < /tmp/nginx.conf.template > /usr/local/nginx/conf/nginx.conf
-    envsubst < /tmp/httppasswd.template > /usr/local/nginx/conf/httppasswd
+
+    if [[ ! -n "${HTTP_PASSWD}" ]]; then
+      echo "HTTP_PASSWD is not exist"
+      sed -i '29,30d;' /usr/local/nginx/conf/nginx.conf 
+    else
+      echo "HTTP_PASSWD is already set"
+      envsubst < /tmp/httppasswd.template > /usr/local/nginx/conf/httppasswd
+    fi
+
     nginx -g 'daemon off;'
 fi
 
